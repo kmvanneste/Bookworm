660b0e313147d87e2b3811fdcd321f370b3ed746
kmvanneste <kmvanneste17@gmail.com>
2020-09-29 21:18:45 -0400

    added code to pages, backend complete

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-//  YOUR NAV COMPONENT HERE
+function Nav() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Link className="navbar-brand" href="/">
+          React Reading List
+        </Link>
+      </nav>
+    );
+  }
 
 export default Nav;
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -31,9 +31,52 @@ class Saved extends Component {
   };
 
   render() {
-    return (
-      // YOUR CODE HERE
-    );
+      return (
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <Jumbotron>
+                <h1 className="text-center">
+                  <strong>(React) Google Books Search</strong>
+                </h1>
+                <h2 className="text-center">Your Saved Books of Interest.</h2>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <Card title="Saved">
+                {this.state.books.length ? (
+                  <List>
+                    {this.state.books.map(book => (
+                      <Book
+                        key={book.id}
+                        title={book.volumeInfo.title}
+                        subtitle={book.volumeInfo.subtitle}
+                        link={book.volumeInfo.infoLink}
+                        authors={book.volumeInfo.authors.join(", ")}
+                        description={book.volumeInfo.description}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                        Button={() => (
+                          <button
+                            onClick={() => this.handleBookDelete(book.id)}
+                            className="btn btn-primary ml-2"
+                          >
+                            Delete
+                          </button>
+                        )}
+                      />
+                    ))}
+                  </List>
+                ) : (
+                  <h2 className="text-center">{this.state.message}</h2>
+                )}
+              </Card>
+            </Col>
+          </Row>
+          <Footer />
+        </Container>
+      );
   }
 }
 
